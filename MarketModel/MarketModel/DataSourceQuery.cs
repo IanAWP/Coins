f634a5359b1ddd857ffc7da035d5eef23ccf0311
IanAWP <mcfaust@gmail.com>
2017-07-04 22:53:51 +0900

    Entity data models

---
+++ MarketModel/MarketModel/DataSourceQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketModel
+{
+    public partial class DataSourceQuery
+    {
+        public DataSourceQuery()
+        {
+            this.Tickers = new HashSet<Ticker>();
+        }
+
+        public int Id { get; set; }
+        public System.DateTime TimeStamp { get; set; }
+
+        public virtual DataSource DataSource { get; set; }
+
+        public virtual ICollection<Ticker> Tickers { get; set; }
+    }
+}
